ad274b33e0bfe9b860d4fe00c2ba57f99901e1d1
Steorra <3njel@proton.me>
2024-10-10 16:19:25 +0200

    fin tp3 (pas test)

--- src/BufferManager.java
+++ src/BufferManager.java
@@ -6,25 +6,27 @@ public class BufferManager {
     private DBConfig config;
     private DiskManager dm;
     private List<CustomBuffer> bufferList;
+    private int time;
 
     //Constructor
     public BufferManager(DBConfig config, DiskManager dm) {
         this.config = config;
         this.dm = dm;
         this.bufferList = new ArrayList<CustomBuffer>(config.getBm_buffercount());
+        this.time = 0;
     }
 
     public CustomBuffer getPage(PageId pid){
 
         for (CustomBuffer cb : bufferList){
             if (pid.equals(cb.getPid())){
-                cb.pin_count++;
+                cb.setPin_count(cb.getPin_count()+1);
                 return cb;
             }
         }
         for (int i = 0; i < config.getBm_buffercount(); i++ ){
             if (bufferList.get(i)==null) {
-                CustomBuffer cb = new CustomBuffer(pid);
+                CustomBuffer cb = new CustomBuffer(pid, config);
                 dm.ReadPage(pid,cb.getBb());
                 bufferList.add(cb);
                 return cb;
@@ -33,70 +35,49 @@ public class BufferManager {
         CustomBuffer candidat = null;
         if (config.getBm_policy().equals("LRU")) {
             for (int i = 0; i < config.getBm_buffercount(); i++) {
-                if (bufferList.get(i).pin_count==0 && bufferList.get(i).time != 0){
+                if (bufferList.get(i).getPin_count()==0 && bufferList.get(i).getTime() != 0){
                     if (candidat==null) candidat = bufferList.get(i);
-                    else if (candidat.time > bufferList.get(i).time) candidat = bufferList.get(i);
+                    else if (candidat.getTime() > bufferList.get(i).getTime()) candidat = bufferList.get(i);
                 }
 
             }
         }else if(config.getBm_policy().equals("MRU")){
             for (int i = 0; i < config.getBm_buffercount(); i++) {
-                if (bufferList.get(i).pin_count == 0 && bufferList.get(i).time != 0) {
+                if (bufferList.get(i).getPin_count() == 0 && bufferList.get(i).getTime() != 0) {
                     if (candidat == null) candidat = bufferList.get(i);
-                    else if (candidat.time < bufferList.get(i).time) candidat = bufferList.get(i);
+                    else if (candidat.getTime() < bufferList.get(i).getTime()) candidat = bufferList.get(i);
                 }
             }
         }
-        if(candidat.dirty_flag) dm.WritePage(candidat.pid, candidat.bb);
-        CustomBuffer cb = new CustomBuffer(pid);
+        if(candidat.isDirty_flag()) dm.WritePage(candidat.getPid(), candidat.getBb());
+        CustomBuffer cb = new CustomBuffer(pid, config);
         bufferList.set(bufferList.indexOf(candidat), cb);
         return cb;
     }//TODO: tester et finir la fonction
 
-
-    //Class du buffer
-    private class CustomBuffer{
-        private ByteBuffer bb;
-        private PageId pid;
-        private int pin_count;
-        private boolean dirty_flag;
-        private int time;
-
-        public CustomBuffer(PageId pid){
-            this.bb = ByteBuffer.allocateDirect(config.getPagesize());
-            this.pid = pid;
-            this.pin_count = 1;
-            this.dirty_flag = false;
-            this.time = 0;
-        }
-
-        //Getters & Setters
-        public ByteBuffer getBb() {
-            return bb;
-        }
-        public void setBb(ByteBuffer bb) {
-            this.bb = bb;
-        }
-        public PageId getPid() {
-            return pid;
-        }
-        public void setPid(PageId pid) {
-            this.pid = pid;
-        }
-        public int getPin_count() {
-            return pin_count;
-        }
-        public void setPin_count(int pin_count) {
-            this.pin_count = pin_count;
-        }
-        public boolean isDirty_flag() {
-            return dirty_flag;
-        }
-        public void setDirty_flag(boolean dirty_flag) {
-            this.dirty_flag = dirty_flag;
+    public void freePage(PageId pid, boolean valdirty){
+        for(CustomBuffer buff : bufferList){
+            if(buff.getPid().equals(pid)){
+                buff.setPin_count(buff.getPin_count()-1);
+                if(valdirty) buff.setDirty_flag(true);
+                if(buff.getPin_count()==0){
+                    time++;
+                    buff.setTime(time);
+                }
+                break;
+            }
         }
     }
 
+    public void setCurrentReplacementPolicy(String policy){
+        config.setBm_policy(policy);
+    }
 
+    void flushBuffer(){
+        for (CustomBuffer buff : bufferList){
+            if (buff.isDirty_flag()) dm.WritePage(buff.getPid(),buff.getBb());
+            buff=new CustomBuffer(null,config);
+        }
+    }
 
 }
---
+++ src/CustomBuffer.java
@@ -0,0 +1,61 @@
+import java.nio.ByteBuffer;
+
+public class CustomBuffer {
+    private ByteBuffer bb;
+    private PageId pid;
+    private int pin_count;
+    private boolean dirty_flag;
+    private int time;
+    private DBConfig config;
+
+    public CustomBuffer(PageId pid, DBConfig config){
+        this.bb = ByteBuffer.allocateDirect(config.getPagesize());
+        this.pid = pid;
+        this.pin_count = 1;
+        this.dirty_flag = false;
+        this.time = 0;
+        this.config = config;
+    }
+
+    //Getters & Setters
+    public ByteBuffer getBb() {
+            return bb;
+        }
+        public void setBb(ByteBuffer bb) {
+            this.bb = bb;
+        }
+        public PageId getPid() {
+            return pid;
+        }
+        public void setPid(PageId pid) {
+            this.pid = pid;
+        }
+        public int getPin_count() {
+            return pin_count;
+        }
+        public void setPin_count(int pin_count) {
+            this.pin_count = pin_count;
+        }
+        public boolean isDirty_flag() {
+            return dirty_flag;
+        }
+        public void setDirty_flag(boolean dirty_flag) {
+            this.dirty_flag = dirty_flag;
+        }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public DBConfig getConfig() {
+        return config;
+    }
+
+    public void setConfig(DBConfig config) {
+        this.config = config;
+    }
+}

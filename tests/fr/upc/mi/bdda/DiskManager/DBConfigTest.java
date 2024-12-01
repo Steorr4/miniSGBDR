a7c32b3a969f75f3b3411e3ed6839b6079afe9db
Steorra <3njel@proton.me>
2024-12-01 21:44:30 +0100

    C'est l'heure des tests unitaires ! ☺️🔫

--- tests/fr/upc/mi/bdda/DiskManager/DBConfigTest.java
+++ tests/fr/upc/mi/bdda/DiskManager/DBConfigTest.java
@@ -2,17 +2,41 @@ package fr.upc.mi.bdda.DiskManager;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DBConfigTest {
     @ParameterizedTest
-    @CsvSource({"'./DB', -1, 4096, 3, 'MRU'", "'./DB', -1024, -12, 3, 'MRU'","'./DB', 1024, 4096, -3, 'MRU'"})
+    @CsvSource({"'./DB', -1, 4096, 3, 'MRU'", "'./DB', 1024, -12, 3, 'MRU'","'./DB', 1024, 4096, -3, 'MRU'"})
     void testIllegalArgumentDBConfig(String dbpath, int pagesize, int dm_maxfilesize,
                                      int bm_buffercount, String bm_policy){
         assertThrows(IllegalArgumentException.class, ()->{
             new DBConfig(dbpath, pagesize, dm_maxfilesize, bm_buffercount, bm_policy);
         });
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings="./fauxchemin")
+    void testMissingFileLoadDBConfig(String fic_config){
+        assertThrows(IOException.class,()->{
+            DBConfig.LoadDBConfig(fic_config);
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource({"'./DB', 1024, 4096, 3, 'MRU'", "'./DB', 3, 9, 15, 'LRU'"})
+    void testGetters(String dbpath, int pagesize, int dm_maxfilesize,
+                                     int bm_buffercount, String bm_policy){
+        DBConfig config = new DBConfig(dbpath, pagesize, dm_maxfilesize, bm_buffercount, bm_policy);
+
+        assertEquals(config.getDbpath(),dbpath);
+        assertEquals(config.getPagesize(),pagesize);
+        assertEquals(config.getDm_maxfilesize(),dm_maxfilesize);
+        assertEquals(config.getBm_buffercount(),bm_buffercount);
+        assertEquals(config.getBm_policy(),bm_policy);
     }
 
 }

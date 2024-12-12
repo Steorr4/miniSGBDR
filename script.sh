95b012f8783eb4079e6b75e98d182ed764e37a31
Steorra <3njel@proton.me>
2024-12-12 13:11:21 +0100

    Updated script

--- script.sh
+++ script.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-compile(){
-javac -cp "lib/*" -d bin src/fr/upc/mi/bdda/*/*.java src/Main.java
-echo "class files compiled"
+
+compile() {
+    javac -cp "lib/*" -d bin src/fr/upc/mi/bdda/*/*.java src/SGBD.java
+    if [ $? -ne 0 ]; then
+        echo "Compilation failed"
+        exit 1
+    fi
+    echo "Compilation successful"
 }
 
-run(){
-java -cp "bin:lib/*" Main
-echo "Launch complete"
+run() {
+    java -cp "bin:lib/*" SGBD "src/config.json"
+    if [ $? -ne 0 ]; then
+        echo "Execution failed"
+        exit 1
+    fi
+    echo "Execution successful"
 }
 
 compile
